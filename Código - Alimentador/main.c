a13579f39a6c624a828c93ff9ed6f67a96ce0e62
enzzocs <csenzzo@gmail.com>
2022-12-21 09:15:23 -0300

    Add files via upload

--- Código - Alimentador/main.c
+++ Código - Alimentador/main.c
@@ -96,20 +96,12 @@ int32_t hx711_read_average() {
 	return sum / 10;
 }
 
-volatile uint8_t flag_tempo = 0;
-
-ISR (TIMER1_COMPA_vect)    // Timer1 ISR
-{
-	flag_tempo = 1;
-	
-}
-
 volatile uint8_t refilling = 0;
 
 ISR (TIMER2_COMPA_vect)
 {
 	if (refilling) {
-		OCR0A += 1;
+		OCR0A += 2;
 	}
 }
 
@@ -163,7 +155,7 @@ time_t get_current_time() {
 		hours += (ds1307_received_data[2] & 0x10) >> 4;
 		uint8_t is_pm = ds1307_received_data[2] & (1 << 5);
 		hours += is_pm ? 12 : 0;
-	} else {
+		} else {
 		hours += (ds1307_received_data[2] & 0x30) >> 4;
 	}
 
@@ -179,28 +171,22 @@ uint32_t current_time_to_timestamp(time_t current_time) {
 }
 
 int main() {
-	OCR1A = 31249;   // for 0.5 sec at 16 MHz
-
-	TCCR1A = 0;
-	TCCR1B = (1<<CS12) | (1<<WGM12);  // Timer mode with 256 prescaler
-	TIMSK1 = (1 << OCIE1A) ;   // Enable timer1 overflow interrupt(TOIE1)
-
+	//controla pwm
 	DDRD |= _BV(PD6);
-	
 	TCCR0A |= _BV(COM0A1) | _BV(WGM00);
 	TCCR0B |= _BV(CS00) | _BV(WGM02);
-
 	TCCR0A |= _BV(WGM20);
+	//incrementa o pwm
 	TCCR2B |= _BV(CS20) | _BV(CS21) | _BV(CS22);
 	TIMSK2 |= _BV(OCIE2A);
-	OCR2A = 91;
-
+	OCR2A = 182; 
+	
 	uint8_t str[MAX_LEN];
 	uint8_t baiti = CARD_NOT_FOUND;
-	OCR0A = 0;
+	OCR0A = 0; //
 	
 	spi_init();
-	//_delay_ms(1000);
+	_delay_ms(1000);
 	mfrc522_init();
 	usart_init();
 	hx711_init();
@@ -208,11 +194,13 @@ int main() {
 	
 	stdout = &uart_output;
 	_delay_ms(10);
+	//Desabilita interrupção do TIMER 2
+	TIMSK2 &= ~_BV(OCIE2A);
+	//Habilita Interrupções Globais
+	sei();
 	
-	//sei();
-	
+	//Debug
 	printf("Starting...\n");
-
 	time_t time = get_current_time();
 	time_t feeding_interval = (time_t){ .hours = 0, .minutes = 3, .seconds = 0 };
 	uint32_t feeding_interval_timestamp = current_time_to_timestamp(feeding_interval);
@@ -221,65 +209,54 @@ int main() {
 	last_feeding_time[1] = timestamp - feeding_interval_timestamp;
 
 	uint8_t refill_target = 0;
-	while(1){
-	baiti = mfrc522_get_serial(str);
-	if (baiti == CARD_FOUND) {
+
+	while (1) {
+		baiti = mfrc522_get_serial(str);
+
+		if (baiti != CARD_FOUND) {
+			continue;
+		}
+		//Debug
 		printf("Card detected: [");
 		for (uint8_t i = 0; i < MAX_LEN; i += 1) {
 			printf("%02X", str[i]);
 		}
 		printf("]\n");
-	}
-	}
-    while (1) {
-		if (flag_tempo) {
-			flag_tempo = 0;
-			
-			time_t time = get_current_time();
 
-			printf("%02d:%02d:%02d:\n", time.hours, time.minutes, time.seconds);			
+		//Cartão encontrado
+		//Verifica Se o cartão esta registrado
+		for (uint8_t i = 0; i < 2; i += 1) {
+			if (!are_tags_equal(tags[i], str)) {
+				printf("%u fail\n", i);
+				continue;
+			}
 
-			if (baiti == CARD_FOUND) {
-				printf("Card detected: [");
-				for (uint8_t i = 0; i < MAX_LEN; i += 1) {
-					printf("%02X", str[i]);
-				}
-				printf("]\n");
-				for (uint8_t i = 0; i < 2; i += 1) {
-					if (!are_tags_equal(tags[i], str)) {
-						printf("%u fail\n", i);
-						continue;
-					}
+			printf("%u success\n", i);
 
-					printf("%u success\n", i);
+			uint32_t current_timestamp = current_time_to_timestamp(time);
+			uint32_t next_feeding_timestamp = last_feeding_time[i] + current_time_to_timestamp(feeding_interval);
+			//Compara se o gato ja pode ser alimentado pelo tempo dês de a ultima alimentação
+			if (current_timestamp >= next_feeding_timestamp) {
+				last_feeding_time[i] = current_timestamp;
+				refilling = 1;
+				refill_target = i;
+				printf("Refilling for %u until %li grams...\n", i, refill_quantities[refill_target]);
+				} else {
+				printf("A refill is still not available for this cat\n");
+			}
+			//Liga o giro do motor ate chegar no peso de ração adequado
+			TIMSK2 |= _BV(OCIE2A);
+			while (1) {
+				int32_t weight = hx711_read_average();
+				printf("Current weight = %li\n\n", weight);
 
-					uint32_t current_timestamp = current_time_to_timestamp(time);
-					uint32_t next_feeding_timestamp = last_feeding_time[i] + current_time_to_timestamp(feeding_interval);					
-
-					if (current_timestamp >= next_feeding_timestamp) {
-						last_feeding_time[i] = current_timestamp;
-						refilling = 1;
-						refill_target = i;
-						printf("Refilling for %u until %li grams...\n", i, refill_quantities[refill_target]);
-					} else {
-						printf("A refill is still not available for this cat\n");
-					}
-
+				if (weight >= refill_quantities[refill_target]) {
 					break;
 				}
 			}
-			
-			int32_t weight = hx711_read_average();
-			printf("Current weight = %li\n\n", weight);
-
-			if (refilling && weight >= refill_quantities[refill_target]) {
-				refilling = 0;
-			}
-
-			baiti = CARD_NOT_FOUND;
+			TIMSK2 &= ~_BV(OCIE2A);
 		}
 		
-		baiti = mfrc522_get_serial(str);
-    }
+	}
 }
-
+
